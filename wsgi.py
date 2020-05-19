c300eec6588e6bdcb5714be0d1045313651afa47
rouyn <rouynxia@gmail.com>
2020-05-18 22:26:09 -0400

    add wsgi

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-from app import (app, db)
-from sites.downloader import DownloadService
-
-if __name__ == '__main__':
-    db.create_all()
-    _downloadSrv = DownloadService()
-    app.run(threaded=True,
-            port=7777)
-    _downloadSrv.shutdown()
